547fd52fad54bdf10dd55c3ed84f2b7782cdf129
amar2708 <amardeep.anand@accionlabs.com>
2017-06-15 21:02:04 +0530

    example for iterators

---
+++ iterators.py
@@ -0,0 +1,24 @@
+# iterators are used to iterate the items of the container. python uses iter()
+# method to return an iterator object of class.The iterator method uses the
+# __next__() method to get the next item. When the iteration is done an
+# StopIteration exception is raised.
+
+
+class Reverse:
+    def __init__(self, name):
+        self.name = name
+        self.index = len(name)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.index == 0:
+            raise StopIteration
+        self.index = self.index-1
+        return self.name[self.index]
+
+if __name__ == "__main__":
+    r = Reverse('amardeep')
+    for char in r:
+        print(char)
